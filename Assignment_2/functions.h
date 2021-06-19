0521acf780c6a1ce58e21ea137ab93eadbf0ad4c
Thomas Nguyen <thomasngn673@gmail.com>
2021-06-19 14:46:35 -0400

    added files for assignment 2

---
+++ Assignment_2/functions.h
@@ -0,0 +1,18 @@
+// Add the include guard.
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct symbol
+{
+    char character;
+    int frequency;
+    string code;
+};
+
+// Write your functions here
---
+++ Assignment_2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+// Include functions.h here
+
+using namespace std;
+
+int main()
+{
+
+    string message,filename;
+    // Define your alphabet variable here. You must use the given symbol structure to create an array or a vector.
+
+    cin >> filename;
+    if(/* Call the readFromFile function here */==0)
+    {
+        cout << "Message: " << message << endl;
+        // Call the createAlphabet function here.
+        cout << "Number of symbols in the alphabet = " << /*Use a variable with the alphabet size here*/ << endl;
+        cout << "Characters in the alphabet = ";
+        // Call the printAlphabet function here.
+        cout << "Number of bits per symbol = " << ceil(log2(/*Use a variable with the alphabet size here*/) << endl;
+        // Call the printHistogram function here.
+        // Call the prinCodes function here.
+    }
+    else
+        cout << "File not found." << endl;
+    return 0;
+}
