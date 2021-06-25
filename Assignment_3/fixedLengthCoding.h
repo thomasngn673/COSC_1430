6f5e897b9ce4993b764bca25ab054608b8f58686
Thomas Nguyen <thomasngn673@gmail.com>
2021-06-25 02:19:56 -0400

    added templates for assignment 3

---
+++ Assignment_3/fixedLengthCoding.h
@@ -0,0 +1,15 @@
+// Add the include guard
+
+// Add the libraries needed by your solution
+
+struct symbol
+{
+    char character;
+    int frequency;
+    string code;
+};
+
+
+// Write the definition of the fixedLengthCoding class
+
+
